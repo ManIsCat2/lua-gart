31ea73b51e0eaf7a34f080edd37cbc8c5fa178b4
ManIsCat2 <maniscat080@gmail.com>
2025-08-09 16:22:55 +0330

    "better" test

--- testes/symeq.lua
+++ testes/symeq.lua
@@ -1,32 +1,36 @@
+local fmt = string.format
+
 local AddEq = 50
 AddEq += 14
-
-print("TEST AddEq (64)", AddEq)
+assert(AddEq == 64, fmt("AddEq Failed, value: %i", AddEq))
+print("AddEq: OK")
 
 local SubEq = 16
 SubEq -= 14
-
-print("TEST SubEq (2)", SubEq)
+assert(SubEq == 2, fmt("SubEq Failed, value: %i", SubEq))
+print("SubEq: OK")
 
 local IdivEq = 6
 IdivEq //= 2
+assert(IdivEq == 3, fmt("IdivEq Failed, value: %i", IdivEq))
+print("IdivEq: OK")
 
-print("TEST IdivEq (3)", IdivEq)
-
-local tableSymeq = {x=0, t2 = {x=43}}
+local tableSymeq = { x = 0, t2 = { x = 43 } }
 tableSymeq.x += 5
-print("TEST TABLE AddEq (5): ",tableSymeq.x)
+assert(tableSymeq.x == 5, fmt("Table Addeq Failed, value: %i", tableSymeq.x))
+print("Table Addeq: OK")
 tableSymeq.t2.x += 5
-print("TEST DOUBLETABLE AddEq (48):", tableSymeq.t2.x)
+assert(tableSymeq.t2.x == 48, fmt("DoubleTable Addeq Failed, value: %i", tableSymeq.t2.x))
+print("DoubleTable Addeq: OK")
 
 
-local function n()
+local function nrt()
     return 1, 2
 end
 
-local n1, n2 =5,0
-
-n1,n2 += n()
-
-print("TEST FUNCRT1 (6): ", n1)
-print("TEST FUNCRT2 (2): ", n2)
+local n1, n2 = 5, 0
+n1, n2 += nrt()
+assert(n1 == 6, fmt("ReturnVal AddEq Failed, value: %i", n1))
+print("ReturnVal Addeq: OK")
+assert(n2 == 2, fmt("ReturnVal2 AddEq Failed, value: %i", n2))
+print("ReturnVal2 Addeq: OK")
